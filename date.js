f3c9422fdc19a3c7ee79ae8097c43f240fcaebea
Ronnie <rix17172@gmail.com>
2016-12-18 20:04:27 +0100

    Update date.js

--- date.js
+++ date.js
@@ -1,1 +1,22 @@
-
+//this file offer a option to use 'date' like php.
+function date(format, timestamp){
+  var i=-1;
+  var time;
+  var buffer = "";
+  function Container(){
+    function Con(test){
+      return typeof this[test] != "undefined";
+    }
+    this.containes = Con;
+  }
+  var container = new Container();
+  while(format.length-1 > i){
+    i++;
+    if(container.containes(format.charAt(i))){
+       buffer += container[format.charAt(i)](time);
+    }else{
+       buffer += format.charAt(i);
+    }
+  }
+  return buffer;
+}
